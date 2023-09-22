791cecf7d31435665776a2921b8fbf5cc97a9ea2
Iamneo <techadmin@iamneo.ai>
2023-09-22 16:13:04 +0000

    Backup Commit

--- main.tf
+++ main.tf
@@ -13,10 +13,15 @@ provider "aws" {
   secret_key = "V7v1TSxtgYYYjuIEzAHDh8N87momjg+QmZ+eBRpD"
 }
 
-resource "aws_instance" "CQ2"
-{
-    
+resource "aws_instance" "CQ2"{
+    ami = "ami-0ed99df77a82560e6"
+    instance_type = "t2.micro" 
 }
+
+output "public_ip"{
+    value = aws_instance.CQ2.public_ip
+}
+
 
 
 
